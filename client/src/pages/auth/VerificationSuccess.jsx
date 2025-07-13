42198098716c69708d146f8565ce1c385df52742
YuveerK <yuveerkal01@gmail.com>
2025-07-13 04:43:55 +0200

    Test 3

--- client/src/pages/auth/VerificationSuccess.jsx
+++ client/src/pages/auth/VerificationSuccess.jsx
@@ -14,8 +14,6 @@ const VerificationSuccess = () => {
       <div className="absolute inset-0 bg-black/60 backdrop-blur-sm z-[-1]" />
 
       {/* Registration Card */}
-      {/* Registration Card */}
-      {/* Registration Card */}
       <div className="flex items-center justify-center w-full h-screen overflow-auto">
         <div className="bg-white p-8 rounded-xl shadow-xl w-full max-w-md border border-gray-200">
           <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
